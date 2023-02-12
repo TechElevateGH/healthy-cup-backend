9f0878fa85c5cbab73d87f37d6e2f776ff90e09d
DaisyAwo <daisyawo27@gmail.com>
2023-02-11 21:33:06 -0800

    Implemented CRUD operations for client entity

--- app/ents/client/crud.py
+++ app/ents/client/crud.py
@@ -3,3 +3,9 @@ from typing import Optional
 from app.core.security import security
 from app.ents.base.crud import CRUDBase
 from app.ents.employee.models import Client
+from app.ents.employee.schema import ClientCreateInput, ClientInDB
+
+
+class ClientCRUD(CRUDBase[Client, ClientInDB]):
+    pass
+crud = ClientCRUD(Client)
