608656bf473b7e4967dc568aeaaa5b7380a055a1
DaisyAwo <daisyawo27@gmail.com>
2023-02-13 21:54:38 -0800

    Modified functions for Client CRUD

--- app/ents/client/crud.py
+++ app/ents/client/crud.py
@@ -13,5 +13,21 @@ class ClientCRUD(CRUDBase[Client, ClientInDB]):
     def read_by_id(self, client_id: str) -> Optional[Client]:
         return super().read_by_id(client_id)
 
+    def read_by_email(self, client_email: str) -> Optional[Client]:
+        return Client.query.filter_by(email=client_email).first()
+
+    def read_multi(self) -> list[Client]:
+        """Read all employees."""
+        return super().read_multi()
+
+    def create(self, client_in: ClientCreateInput) -> Client:
+        """Create a client using `client_in`."""
+        client_in.password = security.hash_password(client_in.password)
+
+        client_obj = ClientInDB(
+            full_name=self.__create_full_name(client_in), **client_in.dict()
+        )
+        return super().create(client_obj)
+
 
 crud = ClientCRUD(Client)
