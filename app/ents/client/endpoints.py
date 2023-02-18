71e85c49878dc4d8dc59691571739d7e999e6b70
DaisyAwo <daisyawo27@gmail.com>
2023-02-18 11:51:42 -0800

    Implemented endpoint to login a client

--- app/ents/client/endpoints.py
+++ app/ents/client/endpoints.py
@@ -4,16 +4,17 @@ from http import HTTPStatus
 from flask import Blueprint, request
 from pydantic import ValidationError
 
-bp = Blueprint("clients", __name__, url_prefix="/clients")
-
 from app.core.security import security
 from app.ents.admin.deps import admin_required
+from app.ents.base.deps import authenticate, is_active
 from app.ents.client.crud import crud as client_crud
 from app.ents.client.schema import (ClientCreateInput, ClientLoginInput, ClientReadClient)
+from app.utilities.errors import MissingLoginCredentials, UserDoesNotExist
 from app.utilities.reponses import (error_response, success_response,
                                     success_response_multi,
                                     validation_error_response)
 
+bp = Blueprint("clients", __name__, url_prefix="/clients")
 
 @bp.post("/")
 def create_client():
@@ -58,6 +59,36 @@ def get_client(client_id: str):
         else error_response(error="Client does not exist.", code=HTTPStatus.NOT_FOUND)
     )
 
+
+@bp.post("/clients/login")
+def employee_client_login():
+    """Log in a client."""
+    try:
+        data = ClientLoginInput.parse_obj(request.form)
+        client = is_active(authenticate(client_crud, data.email, data.password))
+    
+
+        if not (client):
+            return error_response(
+                error=UserDoesNotExist.msg, code=HTTPStatus.BAD_REQUEST
+            )
+
+        response, tokens = None, ("", "")
+
+        if client:
+            tokens = security.create_auth_tokens(client.email)
+            response = ...
+
+        return success_response(
+            data=response,
+            code=HTTPStatus.OK,
+            headers={"Authorization": f"Bearer {tokens[0]}"},
+            cookies={"refresh_token": f"Bearer {tokens[1]}"},
+        )
+    except ValidationError:
+        return error_response(
+            error=MissingLoginCredentials.msg, code=HTTPStatus.UNAUTHORIZED
+        )
 
 @bp.put("/<str:client_id>")
 def update_client(client_id):
